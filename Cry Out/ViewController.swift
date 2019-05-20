f1bf938de8a10c3867d51d94e23f0a3bd86d9a87
Regnauldy Tanri <regnauldyt@gmail.com>
2019-05-20 16:01:10 +0700

    Add UI, Action, Sprite Kit, & BGM

--- Cry Out/ViewController.swift
+++ Cry Out/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import SpriteKit
+import AVFoundation
+import GameKit
 
 class ViewController: UIViewController {
-
+    let emRain = SKSpriteNode(fileNamed: "emitter")
+    var audioPlayer = AVAudioPlayer()
+    let rainSound = URL(fileURLWithPath: Bundle.main.path(forResource: "Light_Rain", ofType: "mp3")!)
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        
     }
+    //BUTTON PLAY
+    @IBAction func btnPlay(_ sender: Any) {
+    //BUAT KASIH HUJAN
+    func rainDrops() {
+        let sceneView = SKView(frame: view.frame)
+        sceneView.backgroundColor = .clear
+        sceneView.allowsTransparency = true
+        self.view.addSubview(sceneView)
+        
+        let scene = SKScene(size: view.bounds.size)
+        scene.backgroundColor = .clear
+        
+        let emitter = SKEmitterNode(fileNamed: "Rain Drops")
+        emitter?.position = sceneView.center
+        scene.addChild(emitter!)
+        sceneView.presentScene(scene)
+        
+     }
+        //MANGGIL HUJAN
+        rainDrops()
+    
+        do {
+        audioPlayer = try AVAudioPlayer(contentsOf: rainSound)
+        audioPlayer.play()
+        } catch{
+            
+        }
 
-
+    
+    }
 }
-
