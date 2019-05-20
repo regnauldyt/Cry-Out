af3f20cb53739b3356595958f04c0a7d40032dda
Regnauldy Tanri <regnauldyt@gmail.com>
2019-05-20 13:21:17 +0700

    Initial Commit

---
+++ Cry Out/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cry Out
+//
+//  Created by Regnauldy Tanri on 20/05/19.
+//  Copyright © 2019 Regnauldy Tanri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
